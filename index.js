f88a2521366c740d536c8ca014f13ddcfe3e51ca
Flavio Wu <flaviowu@gmail.com>
2021-09-06 20:30:57 -0300

    add variaveis de ambiente

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const Conn = require("./models/conn/conn");
 
 const app = express();
-Conn("localhost", 27017, "filmes");
 
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 app.use(express.json());
 
--- models/conn/conn.js
+++ models/conn/conn.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 
-// function Conn(url, porta, banco) {
-//   mongoose
-//     .connect(`mongodb://${url}:${porta}/${banco}`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then(() => {
-//       console.log("MongoDb Conectado");
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
-// module.exports = Conn;
-// 
-
-const Config = require("./config")
-
-function Conn() {
-  mongoose.connect(`mongodb+srv://${Config.user}:${Config.pass}@cluster0.6xyk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDb Conectado");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+function Conn(url, user, pass, banco) {
+  mongoose
+    .connect(`${url}/${banco}`, {
+      user: user,
+      pass: pass,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDb Conectado");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
+module.exports = Conn;
+
+
+// const Config = require("./config")
+
+// function Conn() {
+//   mongoose.connect(`${url}/${banco}`, {
+
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("MongoDb Conectado");
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+// }
 module.exports = Conn;
